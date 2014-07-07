060ec3dc6a610662222fa3dedf462888b9950aa7
unknown <Jipoom@.(none)>
2014-07-07 21:50:11 +0700

    Campaign Deletion

--- app/views/admin/campaign/delete.blade.php
+++ app/views/admin/campaign/delete.blade.php
@@ -9,18 +9,18 @@
         </ul>
     <!-- ./ tabs -->
     {{-- Delete Category Form --}}
-    <form id="deleteForm" class="form-horizontal" method="post" action="@if (isset($category)){{ URL::to('admin/category/' . $category->id . '/delete') }}@endif" autocomplete="off">
+    <form id="deleteForm" class="form-horizontal" method="post" action="@if (isset($campaign)){{ URL::to('admin/campaign/' . $campaign->id . '/delete') }}@endif" autocomplete="off">
         
         <!-- CSRF Token -->
         <input type="hidden" name="_token" value="{{{ csrf_token() }}}" />
-        <input type="hidden" name="id" value="{{ $category->id }}" />
+        <input type="hidden" name="id" value="{{ $campaign->id }}" />
         <!-- <input type="hidden" name="_method" value="DELETE" /> -->
         <!-- ./ csrf token -->
 
         <!-- Form Actions -->
         <div class="form-group">
             <div class="controls">
-                Delete Category
+                Delete Campaign
                 <element class="btn-cancel close_popup"> Cancel</element>
                 <button type="submit" class="btn btn-danger"> Delete</button>
             </div>
