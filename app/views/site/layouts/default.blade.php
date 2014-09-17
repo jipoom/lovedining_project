219e3a8567e3b8a2fdea3971da439a2eed2b3467
unknown <Jipoom@.(none)>
2014-09-17 16:34:58 +0700

    spoon fork position

--- app/views/site/layouts/default.blade.php
+++ app/views/site/layouts/default.blade.php
@@ -247,9 +247,9 @@
 						
 						<a href="{{{ URL::to('/') }}}"><img src={{asset('assets/img/social_gray/fb.png')}} style="width: 25px;"></a></li>
 						<a href="{{{ URL::to('/') }}}"><img src={{asset('assets/img/social_gray/twitter.png')}} style="width: 25px;"></a></li>
-						
+						<a href="{{{ URL::to('/') }}}"><img src={{asset('assets/img/spoonforkfooter.png')}} style="width: 50px;"></a></li>
 					</p>
-					<a href="{{{ URL::to('/') }}}"><img src={{asset('assets/img/spoonforkfooter.png')}} style="width: 50px;"></a></li>
+					
 					<table>
 						<tr>
 							<th class="col-md-4"><a href="{{{ URL::to('/') }}}" style="color:#0D8FA9;">Advertisement</a></li>
