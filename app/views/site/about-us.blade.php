c9588c8980c836dfd6ab7f297fe4add0e43bbef2
NuNing <suriyaporna@gmail.com>
2014-12-25 14:54:01 +0700

    about us border

--- app/views/site/about-us.blade.php
+++ app/views/site/about-us.blade.php
@@ -6,7 +6,7 @@ About Us
 @stop
 @section('content')
 
-<div class="col-md-12">
+<div class="col-md-12" style="border-color: #dedede; border-style: solid; border-width: 1px; border-radius: 5px; padding: 25px; margin: 10px;">
 {{$introduction->content}}
 </div>
 @stop
