b8c45660ce16c9b1422cd0a809b6fa9592292f67
Jipoom <jipoom@gmail.com>
2015-04-17 23:42:56 +0700

    .

--- app/models/PostsUserRead.php
+++ app/models/PostsUserRead.php
@@ -9,7 +9,7 @@ class PostsUserRead extends Eloquent
 	public static function getUnreadReviews($category,$userId)
 	{
 		//return Post::where('category_id', '=', $category -> id) -> count() - PostsUserRead::where('user_id', '=', $userId) -> where('category_id', '=', $category -> id) -> count();
-		$diff = Post::getReviewsByCategoryUnpaginate(null,$category -> id,'created_at','DESC')->count() - PostsUserRead::join('posts_category','posts_user_read.post_id','=','posts_category.post_id')->where('user_id', '=', $userId) -> where('posts_category.category_id', '=', $category -> id) -> count();
+		$diff = Post::getReviewsByCategoryUnpaginate(null,$category -> id,'created_at','DESC')->count() - Post::active()->join('posts_category','posts.id','=','posts_category.post_id')->join('posts_user_read','posts.id','=','posts_user_read.post_id')->where('posts_user_read.user_id', '=', $userId) -> where('posts_category.category_id', '=', $category -> id) -> count();
 		return $diff;
 	
 	}
