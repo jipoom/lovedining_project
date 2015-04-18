ef7ad2d787da04213ba36ec044585531ff4a9906
NuNing <suriyaporna@gmail.com>
2015-04-18 15:05:17 +0700

    voucher

--- app/views/site/campaign/download.blade.php
+++ app/views/site/campaign/download.blade.php
@@ -6,30 +6,49 @@ require_once(public_path().'/mpdf60/mpdf.php');
 
 @section('styles')
 <!-- put style here -->
+<style>
+p {
+        color: #424242;
+        font-family: "Adobe Caslon Pro", "Hoefler Text", Georgia, Garamond, Times, serif;
+	letter-spacing:0.1em;
+	text-align:center;
+	margin: 40px auto;
+	text-transform: lowercase;
+	line-height: 145%;
+	font-size: 14pt;
+	font-variant: small-caps;
+        }	
+	
+</style>
 @stop
 {{-- Content --}}
 @section('content')
 <?php ob_start(); ?>
-<div class="" style="border-style: ridge; border-width: 5px; border-color: #C0C0C0; margin: auto 30px 30px 30px; padding: auto 30px 30px 30px; background-color: #eeeeff;" align="center;">
-<div style="float: left; width: 28%; margin-top: 1cm;"><h1 style="">VOUCHER</h1></div>
-<div style="float: right; width: 28%;"><img src="{{{ asset('assets/img/logo.png') }}}" alt="Logo"  height="150" class ="logo"></div>
-<div style="float: right; width: 28%;">
+<div class="" style="border-style: ridge; border-width: 5px; border-color: #C0C0C0; margin: 0px 30px 0px 30px; padding: 0px 30px 0px 30px; background-size:10%; background-image: url({{{ asset('assets/img/wm.png') }}});"  align="center;">
+<div style="float: left; width: 46%; margin-top: 1cm; margin-bottom: 10px;"><img src="{{{ asset('assets/img/voucher.png') }}}" alt="Voucher" height="200"></div>
+<div style="float: right; position: absolute; right:0px; width: 25%; padding: 0px; margin: 0px;"><img src="{{{ asset('assets/img/logo.png') }}}" alt="Logo"  height="150px" class ="logo"></div>
+<div style="float: right; position: absolute; right:0px; width: 25%; padding: 0px; margin: 0px;">
 @if(!(count(glob(Config::get('app.image_path').'/'.$campaign->album_name.'/banner/'.'*.{jpg,png,gif,JPG,PNG,GIF,jpeg,JPEG}',GLOB_BRACE)) === 0))
 	<?php
 	$banner = Picture::directoryToArray(Config::get('app.image_path').'/'.$campaign->album_name.'/banner/',true);
 	?>
-		<a href="{{$campaign->url()}}"  class="thumbnail"><img src="{{Config::get('app.image_base_url').'/'.$campaign->album_name.'/banner/'.$banner[0]}}" align="middle" height="150"/></a>		
+		<a href="{{$campaign->url()}}"><img src="{{Config::get('app.image_base_url').'/'.$campaign->album_name.'/banner/'.$banner[0]}}" height="150"/></a>		
 
 @else
 		@if($campaign->hotel_logo!="")			
-				<a href="{{{ $campaign->url() }}}" class="thumbnail"><img src="{{Config::get('app.image_base_url').'/'.$campaign->album_name.'/'.$campaign->hotel_logo}}" alt="" height="150"></a>
+				<a href="{{{ $campaign->url() }}}" ><img src="{{Config::get('app.image_base_url').'/'.$campaign->album_name.'/'.$campaign->hotel_logo}}" alt="" height="150"></a>
 		@else
-				<a href="{{{ $campaign->url() }}}" class="thumbnail"><img src="http://placehold.it/260x180" alt="" height="150"></a>
+				<a href="{{{ $campaign->url() }}}"><img src="http://placehold.it/260x180" alt="" height="150"></a>
 		@endif
 @endif
 </div>
+
 <div style="clear: both; margin: 0pt; padding: 0pt; ">
-<div style="font-family: Shruti; font-style: oblique; color: aaaaff;">
+<div style="color: #313131; font-size: 20pt; font-style: oblique; margin: 0px; padding: 0px; line-height: 30%">
+	<p>Campaign name ทดสอง</p>
+</div>
+
+<div style="color: #424242; letter-spacing: 0.1em; font-size: 14pt; line-height: 80%;">
 @if($campaign->show_firstname == 1)
 	<p>
 	First name: {{$userCampaign->user_firstname}}
@@ -43,18 +62,21 @@ require_once(public_path().'/mpdf60/mpdf.php');
 <p>
 Code: {{$userCampaign->campaign_code}}
 </p>
+</div>
+<div style="color: red; font-size: 12pt;">
+Date expired: {{$campaign->expiry_date}}
+</div>
+<div style="float: right; line-height: 50%">
 @if($campaign->remark1 != "")
 	<p>
-	Remark*: {{$campaign->remark1}}
+	* {{$campaign->remark1}}
 	</p>
 @endif
 @if($campaign->remark2 != "")
 	<p>
-	Remark*: {{$campaign->remark2}}
+	** {{$campaign->remark2}}
 	</p>
 @endif
-
-Date expired: {{$campaign->expiry_date}}
 </div>
 </div>
 <?Php
